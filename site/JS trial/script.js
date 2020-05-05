4ba7faf8728279b6b39e1a2ae2f89dab0a86bccc
RohitLobo <48516042+RohitLobo@users.noreply.github.com>
2020-05-05 14:09:20 +0200

    projet LV Factory update

--- site/JS trial/script.js
+++ site/JS trial/script.js
@@ -1,1 +1,28 @@
-var st='hello world';
+//var facebook={
+  //  name:'Facebook',
+  //  ceo:{
+  //  firstName:'mark',
+   // color:'yellow'
+   // },
+   // stock:110,
+   // 'stock of company':100
+    // };
+
+    // console.log(facebook["ceo"]["color"]);//
+
+
+  /*function makeMultiplier(multiplier){
+        var myFunc = function (x){
+            return multiplier * x ;
+        }
+        return myFunc;
+    };
+
+    var multiplyBy3= makeMultiplier (3);
+    console.log(multiplyBy3(10));*/
+    
+
+    var a=7;
+    var b=a;
+    console.log("value of b is:"+ b);
+    console.log("value of a is:"+ a);
