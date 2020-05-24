a83fd5cbd833884a17c38f1c8cb99de063a7181f
RohitLobo <48516042+RohitLobo@users.noreply.github.com>
2020-05-24 23:29:05 +0200

    short sight page

--- site/LV Factory/js/script.js
+++ site/LV Factory/js/script.js
@@ -1,24 +1,39 @@
-let addBtn=document.getElementById("add");
-let subBtn=document.getElementById("subtract");
-var inpBtn=document.querySelector("input");
-
-addBtn.addEventListener('click',countIncrement);
-subBtn.addEventListener('click',countDecrement);
+$(function(){      //document.addEventListener('DOMContentLoaded',function(event){})
 
 
+    $(".navbar-toggler").blur(function(event){                  //queryselector navbar
+        var screenWidth=window.innerWidth;
+        console.log(event)
+        if (screenWidth< 768){
+            $("#menu-toggler").collapse("hide");                //queryselector (menu toggler)
+        }
+    });
 
 
-function countIncrement(){
-    if (inpBtn.value <5){
-        inpBtn.value=parseInt(inpBtn.value,10)+1;
-    }
-}
+});
 
 
-function countDecrement(){
-    if (inpBtn.value>0){
-        inpBtn.value=parseInt(inpBtn.value,10)-1;
-    }
+// let addBtn=document.getElementById("add");
+// let subBtn=document.getElementById("subtract");
+// var inpBtn=document.querySelector("input");
+
+// addBtn.addEventListener('click',countIncrement);
+// subBtn.addEventListener('click',countDecrement);
+
+
+
+
+// function countIncrement(){
+//     if (inpBtn.value <5){
+//         inpBtn.value=parseInt(inpBtn.value,10)+1;
+//     }
+// }
+
+
+// function countDecrement(){
+//     if (inpBtn.value>0){
+//         inpBtn.value=parseInt(inpBtn.value,10)-1;
+//     }
     
-}
+// }
 
