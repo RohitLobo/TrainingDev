20884cfd5534f1aa55e9b1050b0ac8a594bbecc3
RohitLobo <48516042+RohitLobo@users.noreply.github.com>
2020-05-30 20:00:56 +0200

    new gif+ show/hide for cookies

--- site/Bootstrap trial/js/script.js
+++ site/Bootstrap trial/js/script.js
@@ -1,36 +1,50 @@
 $(window).ready(function() {
     $("#cookie-button button").click(function(event){
         $("#cookie-container").hide();
+        $("#popupcard").show();
+
+        var loadBar=document.querySelector("#loading-prog")
+        var widthAug=0;
+        loadBar.style.width='1%';
+       
+        
+        
+        
+        function loadBarAnime(){
+            
+            
+            var loadBarWidth= loadBar.clientWidth;
+            var loadBarWidthPerc=Math.floor(loadBarWidth*0.410) +'%'
+            if(widthAug>100){
+                $("#popupcard").hide();
+                
+            }
+            else{
+                widthAug++;
+                loadBar.style.width=widthAug +'%';
+                loadBar.innerHTML=loadBarWidthPerc;
+                
+                
+            }
+            
+    
+        }
+        
+        setInterval(loadBarAnime,50);
+
+
+
+
+
 
     });
    
     // $("#loading-prog").append($("#loading-prog").width())
      
     
-    var loadBar=document.querySelector("#loading-prog")
-    loadBar.style.width='1%';
     
+   
     
-    var widthAug=0;
-    function loadBarAnime(){
-        console.log('start')
-        
-        var loadBarWidth= loadBar.clientWidth;
-        var loadBarWidthPerc=Math.floor(loadBarWidth*0.410) +'%'
-        if(widthAug>100){
-            console.log('end')
-        }
-        else{
-            widthAug++;
-            loadBar.style.width=widthAug +'%';
-            loadBar.innerHTML=loadBarWidthPerc;
-            
-        }
-        
-
-    }
-    
-    setInterval(loadBarAnime,50);
     
   
 
