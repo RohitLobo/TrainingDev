4e5530d49db2fc7b7ed2319db3242d7aca62b477
RohitLobo <48516042+RohitLobo@users.noreply.github.com>
2020-05-30 17:27:29 +0200

    loadbar complete

--- site/Bootstrap trial/js/script.js
+++ site/Bootstrap trial/js/script.js
@@ -7,7 +7,30 @@ $(window).ready(function() {
     // $("#loading-prog").append($("#loading-prog").width())
      
     
+    var loadBar=document.querySelector("#loading-prog")
+    loadBar.style.width='1%';
     
+    
+    var widthAug=0;
+    function loadBarAnime(){
+        console.log('start')
+        
+        var loadBarWidth= loadBar.clientWidth;
+        var loadBarWidthPerc=Math.floor(loadBarWidth*0.410) +'%'
+        if(widthAug>100){
+            console.log('end')
+        }
+        else{
+            widthAug++;
+            loadBar.style.width=widthAug +'%';
+            loadBar.innerHTML=loadBarWidthPerc;
+            
+        }
+        
+
+    }
+    
+    setInterval(loadBarAnime,50);
     
   
 
